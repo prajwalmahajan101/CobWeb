31af280e64ee97a38235a92b900181476a5bcccb
Prajwal Mahajan <82263793+prajwalmahajan101@users.noreply.github.com>
2022-11-16 19:13:26 +0530

    Some changes for Test

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,7 @@
 import '../styles/App.css';
-import logo from '../assets/Administration_Icon.svg';
 function App() {
   return (
     <div>
-        <img src={logo} alt="logo" />
       Hello World
     </div>
   );
