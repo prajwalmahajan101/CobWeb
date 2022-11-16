a6425794da221034c2c9174e3dcdb4d5c6620d58
Prajwal Mahajan <82263793+prajwalmahajan101@users.noreply.github.com>
2022-11-16 19:07:53 +0530

    Initial Commit

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import '../styles/App.css';
+import logo from '../assets/Administration_Icon.svg';
+function App() {
+  return (
+    <div>
+        <img src={logo} alt="logo" />
+      Hello World
+    </div>
+  );
+}
+
+export default App;
